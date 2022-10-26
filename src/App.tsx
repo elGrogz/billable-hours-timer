61e53ab40cc757e4c3d775c56a42a83bd946bc3a
Gregor Gilchrist <gregorgilchrist@gmail.com>
2022-10-26 20:19:53 +0200

    added firebase auth

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,23 @@ import "./App.css";
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import {
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  sendPasswordResetEmail,
+  signOut,
+} from "firebase/auth";
+import {
+  getFirestore,
+  query,
+  getDocs,
+  collection,
+  where,
+  addDoc,
+} from "firebase/firestore";
 import { getDatabase, push, ref, set } from "firebase/database";
 import { TaskType } from "./types/TaskType";
 
@@ -28,8 +44,29 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
-
 // const analytics = getAnalytics(app);
+
+const googleProvider = new GoogleAuthProvider();
+
+const signInWithGoogle = async () => {
+  // try {
+  //   const res = await signInWithPopup(auth, googleProvider);
+  //   const user = res.user;
+  //   const q = query(collection(db, "users"), where("uid", "==", user.uid));
+  //   const docs = await getDocs(q);
+  //   if (docs.docs.length === 0) {
+  //     await addDoc(collection(db, "users"), {
+  //       uid: user.uid,
+  //       name: user.displayName,
+  //       authProvider: "google",
+  //       email: user.email,
+  //     });
+  //   }
+  // } catch (err) {
+  //   console.error(err);
+  //   alert(err.message);
+  // }
+};
 
 const writeTaskData = (task: TaskType) => {
   set(ref(database, "tasks/" + task.id), {
@@ -60,7 +97,6 @@ const App = () => {
 
   const removeTask = (task: TaskType) => {
     const indexToUpdate = tasks.indexOf(task);
-    console.log("HELLO!");
     let tempTasks = [...tasks];
     tempTasks.splice(indexToUpdate, 1);
     setTasks(tempTasks);
