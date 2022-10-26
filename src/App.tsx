db86f4a177e39014c965fd3f05fccd51d7648dba
Gregor Gilchrist <gregorgilchrist@gmail.com>
2022-10-26 09:07:39 +0200

    added basic post task with firebase

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,54 @@ import Task from "./components/Task";
 import logo from "./logo.svg";
 import "./App.css";
 
-// interface Task {
-//   name: string;
-//   time: number;
-// }
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getDatabase, ref, set } from "firebase/database";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBbumExMpRIX_RZleVb1jXtJ4E0ccCe1bM",
+  authDomain: "billable-hours-timer.firebaseapp.com",
+  projectId: "billable-hours-timer",
+  storageBucket: "billable-hours-timer.appspot.com",
+  messagingSenderId: "78473279696",
+  appId: "1:78473279696:web:0fa08cba0f726d207e06d5",
+  measurementId: "G-BSPFV6QLYY",
+  databaseURL:
+    "https://billable-hours-timer-default-rtdb.europe-west1.firebasedatabase.app/",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+// const analytics = getAnalytics(app);
+
+function writeTaskData(task: any) {
+  set(ref(database, "tasks/" + task.id), {
+    name: task.name,
+    time: task.time,
+  });
+}
 
 function App() {
   const [tasks, setTasks] = useState<{}[]>([]);
 
   const addNewTask = (): void => {
-    const blankTask: {} = {};
+    const blankTask: {} = {
+      id: Date.now(),
+      name: "",
+      time: 0,
+    };
 
     let tempTasks = [...tasks];
     tempTasks.push(blankTask);
     setTasks(tempTasks);
+    writeTaskData(blankTask);
   };
 
   const removeTask = (task: {}) => {
