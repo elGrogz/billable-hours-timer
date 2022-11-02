53e8105f6befbdb1068757366f7e80bc21b544e2
Gregor Gilchrist <gregorgilchrist@gmail.com>
2022-11-02 08:41:57 +0100

    added inital auth context

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,27 @@
+import { createContext, useState } from "react";
+import { signInWithGoogle } from "../utils/firebase";
+
+const UserAuthContext = createContext();
+
+const AuthContext = ({ children }) => {
+  const [user, setUser] = useState({});
+
+  // function to do google auth and get user data back from firebase
+  const handleSignInWithGoogle = async () => {
+    console.log("ONE");
+    const response = await signInWithGoogle();
+    console.log("TWO");
+
+    if (isUser(response)) {
+      console.log("THREE");
+      setDisplayName(response.displayName);
+      console.log("FOUR");
+    }
+  };
+
+  return (
+    <UserAuthContext.Provider value={{ user, handleSignInWithGoogle }}>
+      {children}
+    </UserAuthContext.Provider>
+  );
+};
