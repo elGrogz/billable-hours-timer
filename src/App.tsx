aeb1a60c9ca630da10e80f97dd14c12461cfa58c
Gregor Gilchrist <gregorgilchrist@gmail.com>
2022-11-06 17:02:45 +0100

    context not working for auth state

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,35 @@ import SignupComponent from "./components/SignupComponent";
 import LoginComponent from "./components/LoginComponent";
 import { useUserAuth, UserAuthContextProvider } from "./contexts/AuthContext";
 import { signOutFromGoogle } from "./utils/firebase";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { onAuthStateChanged, User } from "firebase/auth";
+import { auth } from "./utils/firebase";
 
 const App: React.FC = () => {
+  const [user, setUser] = useState<User | null>(null);
+
   useEffect(() => {
     console.log("user: ", user);
   });
 
-  const user = useUserAuth();
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      console.log("auth state changed: ", currentUser);
+      setUser(currentUser);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  // const user = useUserAuth();
 
   return (
     <div>
       <UserAuthContextProvider>
         <div>
-          {user && (
-            // <button onClick={signOutFromGoogle}>Sign out!</button>
-            <div>hello</div>
-          )}
+          {user && <button onClick={signOutFromGoogle}>Sign out!</button>}
         </div>
         <Routes>
           <Route path="/" element={<LoginComponent />} />
