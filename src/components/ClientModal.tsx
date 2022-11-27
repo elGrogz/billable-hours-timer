24ee851af922009c7f184c4c31b790cd65158730
Gregor Gilchrist <gregorgilchrist@gmail.com>
2022-11-27 20:16:58 +0100

    adding add client stuff

--- src/components/ClientModal.tsx
+++ src/components/ClientModal.tsx
@@ -1,3 +1,5 @@
+import { useUserAuth } from "../contexts/AuthContext";
+
 interface ContextProps {
   closeModal: () => void;
 }
@@ -14,10 +16,15 @@ interface ContextProps {
 // };
 
 const ClientModal: React.FC<ContextProps> = ({ closeModal }) => {
+  const addNewClient = async () => {};
+
+  const auth = useUserAuth();
+
   return (
     <div>
       Enter Client name:
       <input type="text" />
+      <button onClick={addNewClient}>Add Client</button>
       <button onClick={closeModal}>Close</button>
     </div>
   );
